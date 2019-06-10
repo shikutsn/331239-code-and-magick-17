bbdb83a5a3136ae255c60bc3dc59f21c9ed053a7
Nikita Shikuts <shikutsn@bk.ru>
2019-06-10 14:39:00 +0300

    Правки по замечаниям

--- js/stat.js
+++ js/stat.js
@@ -9,6 +9,7 @@ var FONT_GAP = 15;
 var BAR_HEIGHT = 150;
 var BAR_WIDTH = 40;
 var BAR_GAP = 50;
+var BAR_ORDINATE = CLOUD_Y + GAP * 2 + FONT_GAP * 3 + BAR_HEIGHT;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -30,6 +31,10 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
+var getBarColor = function (playerName) {
+  return (playerName === 'Вы') ? 'rgba(255, 0, 0, 1)' : 'hsl(240, ' + Math.floor(100 * Math.random()) + '%, 50%)';
+}
+
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
@@ -42,19 +47,16 @@ window.renderStatistics = function (ctx, names, times) {
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < names.length; i++) {
-    var barActualHeight = Math.round(BAR_HEIGHT * (times[i] / maxTime));
+    var currentPlayerName = names[i];
+    var currentPlayerTime = times[i];
+    var barActualHeight = Math.round(BAR_HEIGHT * (currentPlayerTime / maxTime));
     var currentBarX = CLOUD_X + GAP * 2 + (BAR_WIDTH + BAR_GAP) * i;
-    var currentBarY = CLOUD_Y + GAP * 2 + FONT_GAP * 3 + BAR_HEIGHT;
 
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      ctx.fillStyle = 'hsl(240, ' + Math.floor(100 * Math.random()) + '%, 50%)';
-    }
-    ctx.fillRect(currentBarX, currentBarY - barActualHeight, BAR_WIDTH, barActualHeight);
+    ctx.fillStyle = getBarColor(currentPlayerName);
+    ctx.fillRect(currentBarX, BAR_ORDINATE - barActualHeight, BAR_WIDTH, barActualHeight);
 
     ctx.fillStyle = '#000000';
-    ctx.fillText(names[i], currentBarX, currentBarY + FONT_GAP);
-    ctx.fillText(Math.round(times[i]), currentBarX, currentBarY - barActualHeight - FONT_GAP);
+    ctx.fillText(currentPlayerName, currentBarX, BAR_ORDINATE + FONT_GAP);
+    ctx.fillText(Math.round(currentPlayerTime), currentBarX, BAR_ORDINATE - barActualHeight - FONT_GAP);
   }
 };
